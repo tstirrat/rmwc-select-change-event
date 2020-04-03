7699b89b150d58e8a84f640b7a4d21232015abff
Tim Stirrat <tstirrat@google.com>
2020-04-03 14:56:46 -0700

    @rmwc/select 5.7.2

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, act, fireEvent } from '@testing-library/react';
+import { Select } from '@rmwc/select';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const onChange = jest.fn();
+  const { getByText } = render(
+    <Select onChange={onChange} label="My select" options={['true', 'false']} />
+  );
+
+  act(() => {
+    const label = getByText('My select');
+    const wrapper = label.closest('.mdc-select');
+    const select = wrapper.querySelector('select');
+    fireEvent.change(select, {
+      target: { value: 'false' }
+    });
+  });
+
+  expect(onChange).toHaveBeenCalled();
 });
